9a8a90d6d667747c7abccd738fb28b481ee1e871
johnxadams <johnxadams93@gmail.com>
2022-07-07 20:07:48 +0200

    skeleton ni the making

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// library
+import React from 'react';
+
+//styles
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      test
+    </div>
+  );
+}
+
+export default App;
