7e8d47c086296f895c6003e1f933791cab9504b4
johnxadams <johnxadams93@gmail.com>
2022-07-07 20:57:42 +0200

    update data

---
+++ src/data.js
@@ -0,0 +1,11 @@
+let cars = [
+    {
+      "name": "Lotto",
+      "link": "somelink",
+    },
+    {
+        "name": "Pokemon",
+        "link": "somelink",
+      },
+]
+console.log(cars[1].name);
